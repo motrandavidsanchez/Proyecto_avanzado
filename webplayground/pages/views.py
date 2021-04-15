4eb6fb78b09c2896644772b526ebfd3a71e7a60f
Pacho04 <davidsanchezmotran@gmail.com>
2021-04-15 16:32:11 -0300

    Tuneo del formulario

--- webplayground/pages/views.py
+++ webplayground/pages/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
+from .forms import PageForm
 from .models import Page
 
 
@@ -11,17 +12,18 @@ class PagesListView(ListView):
 
 class PageDetailView(DetailView):
     model = Page
+    form_class = PageForm
 
 
 class PageCreateView(CreateView):
     model = Page
-    fields = ['title', 'content', 'order']
+    form_class = PageForm
     success_url = reverse_lazy('pages:pages')
 
 
 class PageUpdateView(UpdateView):
     model = Page
-    fields = ['title', 'content', 'order']
+    form_class = PageForm
     template_name_suffix = '_update_form'
 
     def get_success_url(self):
