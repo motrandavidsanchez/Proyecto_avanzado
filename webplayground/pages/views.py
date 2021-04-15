bcfa2a22b0b995818625d2450512d6a168bbf906
Pacho04 <davidsanchezmotran@gmail.com>
2021-04-15 15:39:49 -0300

    Delete

--- webplayground/pages/views.py
+++ webplayground/pages/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from .models import Page
 
@@ -26,3 +26,8 @@ class PageUpdateView(UpdateView):
 
     def get_success_url(self):
         return reverse_lazy('pages:update', args=[self.object.id]) + '?ok'
+
+
+class PageDeleteView(DeleteView):
+    model = Page
+    success_url = reverse_lazy('pages:pages')
