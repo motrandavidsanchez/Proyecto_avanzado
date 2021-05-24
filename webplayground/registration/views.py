808fdbfd468ebbd2408981cfface9f8d91c1ba23
Pacho04 <davidsanchezmotran@gmail.com>
2021-05-24 16:02:51 -0300

    Se agrego la funcion de registrar. Video69

--- webplayground/registration/views.py
+++ webplayground/registration/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django import forms
 
 
 class SignUpView(CreateView):
@@ -9,3 +10,13 @@ class SignUpView(CreateView):
 
     def get_success_url(self):
         return reverse_lazy('login') + '?register'
+
+    def get_form(self):
+        form = super(SignUpView, self).get_form()
+        form.fields['username'].widget = forms.TextInput(attrs={'class': 'form-control mb-2',
+                                                                'placeholder': 'Nombre de usuario'})
+        form.fields['password1'].widget = forms.PasswordInput(attrs={'class': 'form-control mb-2',
+                                                                     'placeholder': 'Contraseña'})
+        form.fields['password2'].widget = forms.PasswordInput(attrs={'class': 'form-control mb-2',
+                                                                     'placeholder': 'Repite contraseña'})
+        return form
