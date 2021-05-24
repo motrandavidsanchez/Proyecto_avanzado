68c13c7dc3c7018ffd2175e128bd49ce44b29188
Pacho04 <davidsanchezmotran@gmail.com>
2021-05-24 17:32:48 -0300

    Se agrego el email en el formulario de registrarse- Video 70

--- webplayground/registration/views.py
+++ webplayground/registration/views.py
@@ -1,11 +1,12 @@
-from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django import forms
 
+from .forms import UserCreationFormWithEmail
+
 
 class SignUpView(CreateView):
-    form_class = UserCreationForm
+    form_class = UserCreationFormWithEmail
     template_name = 'registration/signup.html'
 
     def get_success_url(self):
@@ -15,6 +16,8 @@ class SignUpView(CreateView):
         form = super(SignUpView, self).get_form()
         form.fields['username'].widget = forms.TextInput(attrs={'class': 'form-control mb-2',
                                                                 'placeholder': 'Nombre de usuario'})
+        form.fields['email'].widget = forms.EmailInput(attrs={'class': 'form-control mb-2',
+                                                              'placeholder': 'Email'})
         form.fields['password1'].widget = forms.PasswordInput(attrs={'class': 'form-control mb-2',
                                                                      'placeholder': 'Contraseña'})
         form.fields['password2'].widget = forms.PasswordInput(attrs={'class': 'form-control mb-2',
